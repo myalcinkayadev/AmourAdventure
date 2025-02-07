c100887c299c570fc367f5b75ab0f18c42c779ec
myalcinkayadev <mehmetyalcinkaya042@gmail.com>
2025-02-07 21:11:02 +0300

    feat: Add basic Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void Update() {
+        
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetAxis("Vertical") > 0.5f) {
+            PlayGame();
+        }
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
