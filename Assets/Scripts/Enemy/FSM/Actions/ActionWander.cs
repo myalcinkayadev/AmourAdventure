75ddbe99ee2eb661dbf278fbc177df37a6c35722
myalcinkayadev <mehmetyalcinkaya042@gmail.com>
2025-02-08 15:07:15 +0300

    feat: add actionwander fsm state and attach to enemies
    * Removed collision between player and enemies

---
+++ Assets/Scripts/Enemy/FSM/Actions/ActionWander.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class ActionWander : FSMAction
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+    [SerializeField] private float wanderTime;
+    [SerializeField] private Vector2 moveRange;
+
+    private Vector3 movePosition;
+    private float timer;
+
+    public override void Act()
+    {
+        if (timer <= 0f)
+        {
+            SetDestination();
+            ResetWanderTimer();
+        }
+
+        MoveTowardsDestination();
+        timer -= Time.deltaTime;
+    }
+
+    private void MoveTowardsDestination()
+    {
+        if (Vector3.Distance(transform.position, movePosition) < 0.5f) return;
+
+        Vector3 direction = (movePosition - transform.position).normalized;
+        transform.Translate(direction * (speed * Time.deltaTime));
+    }
+
+    private void Awake()
+    {
+        ResetWanderTimer();
+        SetDestination();
+    }
+
+    private void SetDestination() {
+        float randomX = Random.Range(-moveRange.x, moveRange.x);
+        float randomY = Random.Range(-moveRange.y, moveRange.y);
+        movePosition = transform.position + new Vector3(randomX, randomY);
+    }
+
+    private void ResetWanderTimer()
+    {
+        timer = wanderTime;
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (moveRange == Vector2.zero) return;
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, moveRange * 2f);
+        Gizmos.DrawLine(transform.position, movePosition);
+        Gizmos.DrawSphere(movePosition, 0.2f);
+    }
+}
